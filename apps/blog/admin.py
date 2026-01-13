2e0774eb5a7360d1f229086e35811af8de88a6da
k.barylnikov <ipt_git_bot@ipt.ru>
2026-01-13 12:32:36 +0300

    10.1,2 django-mptt и django-mptt-admin

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
+from django_mptt_admin.admin import DjangoMpttAdmin
 
-from apps.blog.models import Post
+from apps.blog.models import Post, Category
+
+
+@admin.register(Category)
+class CategoryAdmin(DjangoMpttAdmin):
+    """
+    Админ-панель модели категорий
+    """
+
+    prepopulated_fields = {"slug": ("title",)}
 
 
 admin.site.register(Post)
--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import FileExtensionValidator
 from django.contrib.auth.models import User
+from mptt.models import MPTTModel, TreeForeignKey
 
 
 class Post(models.Model):
@@ -14,6 +15,12 @@ class Post(models.Model):
     slug = models.SlugField(verbose_name="URL", max_length=255, blank=True, unique=True)
     description = models.TextField(verbose_name="Краткое описание", max_length=500)
     text = models.TextField(verbose_name="Полный текст записи")
+    category = TreeForeignKey(
+        "Category",
+        on_delete=models.PROTECT,
+        related_name="posts",
+        verbose_name="Категория",
+    )
     thumbnail = models.ImageField(
         default="default.jpg",
         verbose_name="Изображение записи",
@@ -58,4 +65,45 @@ class Post(models.Model):
         verbose_name_plural = "Статьи"
 
     def __str__(self):
+        return self.title
+
+
+class Category(MPTTModel):
+    """
+    Модель категорий с вложенностью
+    """
+
+    title = models.CharField(max_length=255, verbose_name="Название категории")
+    slug = models.SlugField(max_length=255, verbose_name="URL категории", blank=True)
+    description = models.TextField(verbose_name="Описание категории", max_length=300)
+    parent = TreeForeignKey(
+        "self",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        db_index=True,
+        related_name="children",
+        verbose_name="Родительская категория",
+    )
+
+    class MPTTMeta:
+        """
+        Сортировка по вложенности
+        """
+
+        order_insertion_by = ("title",)
+
+    class Meta:
+        """
+        Название модели в админ панели, таблица с данными
+        """
+
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        db_table = "app_categories"
+
+    def __str__(self):
+        """
+        Возвращение заголовка категории
+        """
         return self.title
