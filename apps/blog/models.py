1bdefa9159081bacd699f79513e345607fc9ffdc
k.barylnikov <ipt_git_bot@ipt.ru>
2026-01-18 13:56:34 +0300

    10.4 загрузка media в папки с датой

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -25,7 +25,7 @@ class Post(models.Model):
         default="default.jpg",
         verbose_name="Изображение записи",
         blank=True,
-        upload_to="images/thumbnails/",
+        upload_to="images/thumbnails/%Y/%m/%d/",
         validators=[
             FileExtensionValidator(
                 allowed_extensions=("png", "jpg", "webp", "jpeg", "gif")
