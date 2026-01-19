9bfcf33bf73f65d1cebbddea3e204d8dd187d4a3
k.barylnikov <ipt_git_bot@ipt.ru>
2026-01-19 14:14:13 +0300

    10.6. prepopulated_fields и уникальное значение поля slug

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from mptt.models import MPTTModel, TreeForeignKey
 
+from apps.services.utils import unique_slugify
+
 
 class Post(models.Model):
     """
@@ -13,7 +15,7 @@ class Post(models.Model):
     STATUS_OPTIONS = (("published", "Опубликовано"), ("draft", "Черновик"))
 
     title = models.CharField(verbose_name="Название записи", max_length=255)
-    slug = models.SlugField(verbose_name="URL", max_length=255, blank=True, unique=True)
+    slug = models.SlugField(verbose_name="URL", max_length=255, blank=True)
     description = models.TextField(verbose_name="Краткое описание", max_length=500)
     text = models.TextField(verbose_name="Полный текст записи")
     category = TreeForeignKey(
@@ -73,6 +75,13 @@ class Post(models.Model):
         Получаем прямую ссылку на статью
         """
         return reverse('post_detail', kwargs={'slug': self.slug})
+
+    def save(self, *args, **kwargs):
+        """
+        При сохранении генерируем слаг и проверяем на уникальность
+        """
+        self.slug = unique_slugify(self, self.title, self.slug)
+        super().save(*args, **kwargs)
 
 
 class Category(MPTTModel):
