9598241586758dc4f23d5b9498f88888391ee908
k.barylnikov <ipt_git_bot@ipt.ru>
2026-01-20 15:08:29 +0300

    10.3.1.  ListView Представление для отображения записей по категориям

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-from .models import Post
+from .models import Post, Category
 
 
 class PostListView(ListView):
@@ -21,4 +21,23 @@ class PostDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = self.object.title
+        return context
+
+
+class PostFromCategory(ListView):
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    category = None
+
+    def get_queryset(self):
+        self.category = Category.objects.get(slug=self.kwargs['slug'])
+        queryset = Post.objects.filter(category=self.category)
+        if not queryset:
+            sub_cat = Category.objects.filter(parent=self.category)
+            queryset = Post.objects.filter(category__in=sub_cat)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = f'Записи из категории: {self.category.title}'
         return context
