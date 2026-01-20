34f9fa9e1f899fd26532d7f4c99a59a2256e105f
k.barylnikov <ipt_git_bot@ipt.ru>
2026-01-20 15:01:23 +0300

    10.6 бутстрап через CDN

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -25,7 +25,7 @@ class Post(models.Model):
         verbose_name="Категория",
     )
     thumbnail = models.ImageField(
-        default="default.jpg",
+        default="default.png",
         verbose_name="Изображение записи",
         blank=True,
         upload_to="images/thumbnails/%Y/%m/%d/",
