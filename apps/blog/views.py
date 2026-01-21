2945804fd25636dc412000dbbc0175576824a486
k.barylnikov <ipt_git_bot@ipt.ru>
2026-01-22 00:59:59 +0300

    10.11. select_related оптимизация sql

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -7,6 +7,7 @@ class PostListView(ListView):
     template_name = "blog/post_list.html"
     context_object_name = "posts"
     paginate_by = 2
+    queryset = Post.custom.all()  # Переопределение вызова модели
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -20,7 +21,6 @@ class PostDetailView(DetailView):
     model = Post
     template_name = "blog/post_detail.html"
     context_object_name = "post"
-    queryset = Post.custom.all()  # Переопределение вызова модели
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
