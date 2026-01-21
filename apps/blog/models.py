360593ce18b99765f34ff6a162ab4b5ea5a9fb25
k.barylnikov <ipt_git_bot@ipt.ru>
2026-01-21 14:37:32 +0300

    10.9. кастомный менеджер для модели Post

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -7,6 +7,18 @@ from mptt.models import MPTTModel, TreeForeignKey
 from apps.services.utils import unique_slugify
 
 
+class PostManager(models.Manager):
+    """
+    Кастомный менеджер для модели постов
+    """
+
+    def get_queryset(self):
+        """
+        Список постов (SQL запрос с фильтрацией по статусу опубликованно)
+        """
+        return super().get_queryset().filter(status='published')
+
+
 class Post(models.Model):
     """
     Модель постов для нашего блога
@@ -59,6 +71,9 @@ class Post(models.Model):
         blank=True,
     )
     fixed = models.BooleanField(verbose_name="Прикреплено", default=False)
+
+    objects = models.Manager()
+    custom = PostManager()
 
     class Meta:
         db_table = "blog_post"
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -20,6 +20,7 @@ class PostDetailView(DetailView):
     model = Post
     template_name = "blog/post_detail.html"
     context_object_name = "post"
+    queryset = Post.custom.all()  # Переопределение вызова модели
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -35,10 +36,10 @@ class PostFromCategory(ListView):
 
     def get_queryset(self):
         self.category = Category.objects.get(slug=self.kwargs["slug"])
-        queryset = Post.objects.filter(category__slug=self.category.slug)
+        queryset = Post.custom.filter(category__slug=self.category.slug)
         if not queryset:
             sub_cat = Category.objects.filter(parent=self.category)
-            queryset = Post.objects.filter(category__in=sub_cat)
+            queryset = Post.custom.filter(category__in=sub_cat)
         return queryset
 
     def get_context_data(self, **kwargs):
